370797a3e9ccf363263496b22187c9ca1a394c09
Björn Hempel <bjoern@hempel.li>
2025-01-11 18:39:12 +0100

    Upgrade storybook to ReactJS version 18.3.1 to avoid warnings

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+/* Export main components. */
 export { WorldMap } from './components/WorldMap/WorldMap';
+
+/* Export helper components. */
+export { Logo } from './helper/Logo/Logo';
 export { Versions } from './helper/Versions/Versions';
