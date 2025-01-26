8757423850e2dff7ebf25e75121ee9ac6ebe32eb
Björn Hempel <bjoern@hempel.li>
2025-01-26 03:27:58 +0100

    set som zoom bounding box factors and crop bounding box settings for some countries

--- src/components/WorldMap/config/countries.ts
+++ src/components/WorldMap/config/countries.ts
@@ -4,10 +4,16 @@ import {TypeCropBoundingBox, TypeZoomGapBoundingBoxFactor} from "../types/types"
 /* Crop bounding box. */
 export const countryCropBoundingBox: Record<string, TypeCropBoundingBox> = {
 
-    /* Zoom bounding box for france. */
+    /* Zoom bounding box for France. */
     fr: [
         [-5, 40], /* longitude and latitude min. */
         [10, 52]  /* longitude and latitude max. */
+    ],
+
+    /* Zoom bounding box for the Netherlands. */
+    nl: [
+        [3, 50], /* longitude and latitude min. */
+        [8, 54]  /* longitude and latitude max. */
     ],
 
     /* Zoom bounding box for the United States. */
@@ -20,6 +26,24 @@ export const countryCropBoundingBox: Record<string, TypeCropBoundingBox> = {
 /* Zoom bounding box gap. */
 export const zoomGapBoundingBoxFactor: Record<string, TypeZoomGapBoundingBoxFactor> = {
 
-    /* Zoom bounding box for liechtenstein. */
-    li: [20., 20.]
+    /* Zoom bounding box for Andorra. */
+    ad: [20., 20.],
+
+    /* Zoom bounding box for Andorra. */
+    be: [1., 1.],
+
+    /* Zoom bounding box for Liechtenstein. */
+    li: [20., 20.],
+
+    /* Zoom bounding box for Liechtenstein. */
+    lu: [10., 10.],
+
+    /* Zoom bounding box for Monaco. */
+    mc: [80., 80.],
+
+    /* Zoom bounding box for the Netherlands. */
+    nl: [.4, .4],
+
+    /* Zoom bounding box for San Marino. */
+    sm: [40., 40.]
 }
