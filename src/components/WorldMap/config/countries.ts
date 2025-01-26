5a8fd13d854664a6dbfea134060d16d9870c8c15
Björn Hempel <bjoern@hempel.li>
2025-01-26 02:44:10 +0100

    Add zoom bounding box for the United States

--- src/components/WorldMap/config/countries.ts
+++ src/components/WorldMap/config/countries.ts
@@ -9,4 +9,10 @@ export const countryCropBoundingBox: Record<string, TypeShowBoundingBox> = {
         [-5, 40], /* longitude and latitude min. */
         [10, 52]  /* longitude and latitude max. */
     ],
+
+    /* Zoom bounding box for the United States. */
+    us: [
+        [-180, 24], /* longitude and latitude min. */
+        [-66, 72]  /* longitude and latitude max. */
+    ],
 }
