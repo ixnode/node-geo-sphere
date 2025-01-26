79659675e7a78e7d717f7e160007a990620a485d
Björn Hempel <bjoern@hempel.li>
2025-01-26 02:30:45 +0100

    Add zoom bounding box for france

--- src/components/WorldMap/config/countries.ts
+++ src/components/WorldMap/config/countries.ts
@@ -3,10 +3,10 @@ import {TypeShowBoundingBox} from "../types/types";
 
 /* Crop bounding box. */
 export const countryCropBoundingBox: Record<string, TypeShowBoundingBox> = {
-    
+
     /* Zoom bounding box for france. */
     fr: [
-        [-5, 40],
-        [10, 52]
+        [-5, 40], /* longitude and latitude min. */
+        [10, 52]  /* longitude and latitude max. */
     ],
 }
